6b9ae169cd7bb5e22479ae5d8f82d9904b184d08
Manish <manish.na@kpn.com>
2023-10-29 11:42:16 +0530

    Basic CRUD in FastAPI

--- books.py
+++ books.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 BOOKS = [
     {"title":"Book 1","author":"Manish","category":"math"},
     {"title":"Book 2","author":"Raj","category":"science"},
@@ -24,10 +24,29 @@ async def read_science_books():
     return list(filter(lambda item: item["category"] == "science",BOOKS))
 
 # Dynamic Endpoint - Such endpoints must be declared below static ones
-@app.get("/books/{category}")
+@app.get("/books/{category}") #Path Parameter
 async def category_books(category: str):
     return list(filter(lambda item: item["category"] == str(category),BOOKS))
 
-@app.get("/books/")
+@app.get("/books/") #Query Parameter
 async def books_by_author(author:str):
     return list(filter(lambda item: item["author"] == author,BOOKS))
+
+@app.post("/books/create_book")
+async def create_book(new_book=Body()):
+    BOOKS.append(new_book)
+
+    return BOOKS
+
+@app.put("/books/update_book")
+async def update_book(updated_book=Body()):
+    for i in range(len(BOOKS)):
+        if BOOKS[i].get('title').casefold() == updated_book.get('title').casefold():
+            BOOKS[i] = updated_book
+            
+    return BOOKS
+
+@app.delete("/books/delete_book")
+async def delete_book(deleted_book=Body()):
+    BOOKS.remove(list(filter(lambda item: item["title"] == deleted_book["title"],BOOKS))[0])
+    return BOOKS
